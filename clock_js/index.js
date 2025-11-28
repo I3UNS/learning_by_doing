c7fab223190d6e35a6f4145a952007957ff7b623
I3UNS <ashadul9876@gmail.com>
2025-11-28 20:05:12 +0600

    Completed Project Clock JS

--- clock_js/index.js
+++ clock_js/index.js
@@ -9,25 +9,92 @@ let canvas;
 let context;
 
 window.onload = function(){
-    canvas = document.getElementById("canvas");
+    canvas = document.getElementById("clock");
     canvas.width = width;
     canvas.height = height;
     context = canvas.getContext("2d");
 
     context.translate(radius, radius);
+    radius *= 0.9;
     drawClock();
 }
 
 function drawClock(){
     drawFace();
+    drawNumbers();
+    drawTime();
+    setTimeout(drawClock, 1000); //Function is being recalled every second. 1000ms = 1s
 }
 
 function drawFace(){
     context.beginPath();
     context.arc(0, 0, radius, 0, 2*Math.PI);
 
-    context.strokeStyle = "#058743";
+    context.strokeStyle = "black";
     context.lineWidth = radius*0.1;
     context.stroke();
+
+    context.fillStyle = "whitesmoke";
+    context.fill();
 }
 
+function drawNumbers(){
+    context.font = `${radius*0.2}px times new roman`;
+    context.textBaseline = "middle";
+    context.textAlign = "center";
+    context.fillStyle = "black";
+    
+    for (let num = 1; num <= 12; num++){
+        
+        let angle = num * Math.PI / 6;
+        context.rotate(angle);
+        context.translate(0, -radius*0.85);
+        context.rotate(-angle);
+        
+        context.fillText(num.toString(), 0, 0);
+        
+        context.rotate(angle);
+        context.translate(0, radius*0.85);
+        context.rotate(-angle);
+    }
+}
+
+function drawTime(){
+    let now = new Date;
+    let hour = now.getHours() % 12;
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    let secondAngle = second * Math.PI/30; //60 seconds * PI/30 = 2*PI
+
+    context.strokeStyle = "red";
+    drawHand(secondAngle, radius*0.70, radius*0.01);
+
+    let minuteAngle = minute * Math.PI/30; //60 minutes * PI/30 = 2*PI
+    context.strokeStyle = "black";
+    drawHand(minuteAngle, radius*0.70, radius*0.01);
+
+    let hourAngle = hour * Math.PI/6; //12 hours * PI/6 = 2*PI
+    hourAngle += minuteAngle/12;
+    context.strokeStyle = "black";
+    drawHand(hourAngle, radius*0.60, radius*0.02);
+
+    context.beginPath();
+    context.arc(0,0, radius*0.03, 0, 2*Math.PI);
+    context.fillStyle = "black";
+    context.fill();
+}
+
+function drawHand(angle, length, width){
+    context.beginPath();
+    context.lineWidth = width;
+    context.lineCap = "round";
+
+    context.moveTo(0,0);
+    context.rotate(angle);
+    context.lineTo(0, -length);
+
+    context.stroke();
+    context.rotate(-angle);
+
+}
