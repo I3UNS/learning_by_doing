d6d21c9ae910401ad54bc6e89a672b4f585b5645
I3UNS <mohd.assad9876@gmail.com>
2026-01-04 20:46:48 +0600

    Uploaded button_designs

--- button_designs/button-designs.js
+++ button_designs/button-designs.js
@@ -5,15 +5,12 @@
 
 const container = document.getElementById("container");
 const normalBtn = document.getElementById("change-btn");
+const button = document.querySelector("button");
 
 // Normal Button Interactions
 // Button changes the style of its own design
 
-normalBtn.addEventListener("click", changeStyle());
-
-function changeStyle(){
-    // Changes the style with a randomizer
-}
+normalBtn.addEventListener("click", changeStyle);
 
 // Dynamic Button Creation and Interactions
 // Button will be created and styled every click
@@ -23,3 +20,17 @@ dynamicBtn.textContent = "My button and its design has been created dynamically"
 dynamicBtn.style.backgroundColor = "rgb(126,100, 90)"; //rgba would be more appropriate for better variety and look.
 
 container.appendChild(dynamicBtn);
+
+dynamicBtn.addEventListener("click", changeStyle);
+
+function changeStyle(){
+    // Changes the style with a randomizer
+    let rngRed = Math.floor(Math.random()* 256);
+    let rngBlue = Math.floor(Math.random()* 256);
+    let rngGreen = Math.floor(Math.random()* 256);
+    let rngAlpha = Math.random()* 1.0 + 0.5;
+
+    const btnStyle = button.style;
+    btnStyle.backgroundColor = `rgba(${rngRed},${rngBlue},${rngGreen}, ${rngAlpha})`;
+    btnStyle.boxShadow = "10px 5px 10px black";
+}
